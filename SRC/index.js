14e456419ba202afb33915b019e5f53cb525cab6
vitorvarc01 <vitorvarc@gmail.com>
2023-12-15 21:25:28 -0300

    primeirocommite

---
+++ SRC/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require(`express`)
+const knex = require(`./conexão`)
+const cors = require('cors')
+
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/', async (req, res) => {
+    try {
+        const carros = await knex('carros')
+        return res.status(200).json(carros)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json(error)
+    }
+})
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT)
+
+console.log(PORT)
